47bd4d9441bf13eb7c95a063bb9580c2f936944f
claraTang <chialing.tang@soundcloud.com>
2022-05-08 14:22:44 +0200

    Initial Commit

---
+++ yassir/yassirApp.swift
@@ -0,0 +1,17 @@
+//
+//  yassirApp.swift
+//  yassir
+//
+//  Created by Chia Ling Tang on 08.05.22.
+//
+
+import SwiftUI
+
+@main
+struct yassirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
